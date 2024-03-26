0dadac4c9214a949d3d4138e07baf801efcee8c3
kazol1 <163251647+kazol1@users.noreply.github.com>
2024-03-26 12:54:58 +0530

    Create NavBar.js

---
+++ WK_2_D_3/src/components/NavBar.js
@@ -0,0 +1,41 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+    return (
+        <>
+            <Navbar expand='lg' className='bg-body-tertiary'>
+                <Container fluid>
+                    <Navbar.Brand href='#'>ABC Automobiles</Navbar.Brand>
+                    <Navbar.Toggle aria-controls='navbarScroll' />
+                    <Navbar.Collapse id='navbarScroll'>
+                        <Nav
+                            className='me-auto my-2 my-lg-0'
+                            style={{ maxHeight: "100px" }}
+                            navbarScroll>
+                            <Nav.Link as={Link} to='/'>
+                                Home
+                            </Nav.Link>
+                            <Nav.Link as={Link} to='/about-us'>
+                                About us
+                            </Nav.Link>
+                            <Nav.Link as={Link} to='/'>
+                                Vehicles
+                            </Nav.Link>
+                            <Nav.Link as={Link} to='/'>
+                                Services
+                            </Nav.Link>
+                            <Nav.Link as={Link} to='/'>
+                                Contact us
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
+    );
+};
+
+export default NavBar;
